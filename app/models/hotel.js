22df7cae44b9cd5b2c15404c5229701bef4069d7
Wanny Miarelli <wannymiarelli@live.com>
2016-05-20 14:49:11 +0200

    Corretto nome variabile per occupazione totale

--- app/models/hotel.js
+++ app/models/hotel.js
@@ -282,12 +282,12 @@ module.exports = function(){
               //controllo se esiste il giorno
               if(dates[arrival.format('M')][arrival.day()]){
                 dates[arrival.format('M')]['occupancy'] += 1;
-                dates[arrival.format('M')][arrival.day()]['totalOccupancy'] += 1;
+                dates[arrival.format('M')][arrival.day()]['occupancy'] += 1;
                 dates[arrival.format('M')][arrival.day()]['billed'] += reservation.adr;
                 dates[arrival.format('M')][arrival.day()]['adr'] = dates[arrival.format('M')][arrival.day()]['billed'] / dates[arrival.format('M')][arrival.day()]['occupancy'];
               }else{
-                dates[arrival.format('M')][arrival.day()]['totalOccupancy'] = 1;
-                dates[arrival.format('M')]['occupancy'] = 1;
+                dates[arrival.format('M')][arrival.day()]['occupancy'] = 1;
+                dates[arrival.format('M')]['totalOccupancy'] = 1;
                 dates[arrival.format('M')][arrival.day()]['billed'] = reservation.adr;
               }
 
